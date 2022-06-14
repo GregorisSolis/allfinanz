b202f84ec1670b504ca8ed142bf68c9a3dfe81ea
Gregoris Solis <solisgregorip@gmail.com>
2022-06-14 17:15:32 -0300

    le quite el lowercase al nombre de la tarjeta

--- server/src/app/models/card.js
+++ server/src/app/models/card.js
@@ -1,7 +1,7 @@
 const mongoose = require('../../database')
 
 const CardSchema = new mongoose.Schema({
-	name: { type: String, required: true, lowercase: true},
+	name: { type: String, required: true},
 	user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
 	color: { type: String, required: true, lowercase: true},
 	colorFont: { type: String, required: true, lowercase: true},
