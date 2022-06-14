047b8ec92c9bbe593d745d7a25938b7a8acc32eb
Gregoris Solis <solisgregorip@gmail.com>
2022-06-14 14:47:07 -0300

    realizando test con jest

--- allfinanz-web/src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export function Footer(){
-	return(
-		<div className="w-full absolute bottom-0 bg-brand-800 text-center">
-			<span className="uppercase text-white opacity-25 text-sm">allfinanz - creado por Gregorisdev</span>
-		</div>
-	) 
-}
