39f49bc84bc4402ae8b362c77f93170825e39ff2
Gregoris Solis <solisgregorip@gmail.com>
2025-06-23 09:08:49 -0300

    ✨ Atualiza as opções de categorias e tipos de pagamento, adicionando descrições detalhadas e novas opções, melhorando a clareza e a usabilidade do sistema de gerenciamento financeiro.

--- allfinanz-web/src/services/typePayOptions.ts
+++ allfinanz-web/src/services/typePayOptions.ts
@@ -1,5 +1,7 @@
 export const typePayOptions = [
-	{_id: 0,value: 'credit', name: 'Credito'},
-	{_id: 1,value: 'debit', name: 'Debito'},
-	{_id: 2,value: 'dinero', name: 'Dinero'},
-]
+	{ _id: 0, value: '', name: 'Selecione' },
+	{ _id: 1, value: 'credit', name: 'Crédito' },
+	{ _id: 2, value: 'debit', name: 'Débito' },
+	{ _id: 3, value: 'dinheiro', name: 'Dinheiro' },
+	{ _id: 4, value: 'pix', name: 'PIX' },
+];
